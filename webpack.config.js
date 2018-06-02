1ba0d8164161f04b0d190d534ff23ef051ab4e58
subaru44k <odsubaru@gmail.com>
2018-06-02 14:20:17 +0900

    Revert unexpected commit

--- webpack.config.js
+++ webpack.config.js
@@ -43,8 +43,8 @@ module.exports = {
       ],
       // Webpackで利用するときの設定
       alias: {
-//        vue: 'vue/dist/vue.min.js'
-        vue: 'vue/dist/vue.js'
+        vue: 'vue/dist/vue.min.js'
+//        vue: 'vue/dist/vue.js'
       }
     },
   };
