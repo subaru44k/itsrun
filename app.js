e147e38b512aa1d030c9d3da8edd2768e63a999c
subaru44k <odsubaru@gmail.com>
2018-06-02 14:17:47 +0900

    Disable manage page

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/manage', manageRouter);
+//app.use('/manage', manageRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
