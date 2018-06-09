30fb8f886b3a66731fc1fbfd43be8ee1e72843b3
subaru44k <odsubaru@gmail.com>
2018-06-10 00:02:11 +0900

    include firebase as a module

--- src/firebase/FirebaseControl.ts
+++ src/firebase/FirebaseControl.ts
@@ -1,44 +1,51 @@
+import * as firebase from 'firebase/app';
 import { StadiumInfo } from '../model/stadiuminfo';
 
 export class FirebaseControl {
-    private db: any;
+    private db: firebase.firestore.Firestore;
     private defaultCollectionName: string = 'default';
     private stadiumCollectionName: string = 'stadium_info';
     private availabilityCollectionName: string = 'availability';
     private dateCollectionName: string = 'date';
-    constructor(private firebase: any) {
+    constructor(firebase) {
         this.db = firebase.firestore()
     }
 
     getDefaultPageId(): Promise<string> {
         return this.db.collection(this.defaultCollectionName).doc('0').get().then(document => {
             if (document.exists) {
-                return document.data()['alias_id'];
-            } else {
-                console.warn('cannot find default id');
-                return '0';
+                let data = document.data();
+                if (data !== undefined) {
+                    return data['alias_id']
+                }
             }
+            console.warn('cannot find default id');
+            return '0';
         })
     }
 
     getTimeRange(id: string): Promise<Array<string>> {
         return this.getStadiumDocument(id).get().then(document => {
             if (document.exists) {
-                return document.data()['time_range'];
-            } else {
-                console.warn('stadium not found. id: ' + id);
-                return [];
+                let data = document.data();
+                if (data != undefined) {
+                    return data['time_range'];
+                }
             }
+            console.warn('stadium not found. id: ' + id);
+            return [];
         })
     }
 
     getStatusInRange(id: string, dateId: string, rangeIndex: number): Promise<number> {
         return this.getDateDocument(id).doc(dateId).get().then(document => {
             if (document.exists) {
-                return Number(document.data()['status'][rangeIndex]);
-            } else {
-                return 0;
+                let data = document.data();
+                if (data != undefined) {
+                    return Number(data['status'][rangeIndex]);
+                }
             }
+            return 0;
         }).catch((err) => {
             console.warn(err);
             return 0;
