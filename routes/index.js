e787edf4f43eed2810a7cec8c5a2007b15a5e962
subaru44k <odsubaru@gmail.com>
2018-06-11 22:37:59 +0900

    Update title to include seach words. Also modify description.

--- routes/index.js
+++ routes/index.js
@@ -3,43 +3,45 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  rendering('oda_field', '0', res, 0);
+  rendering('oda_field', '織田フィールド', '0', res, 0);
 });
 
 router.get('/oda_field', function(req, res, next) {
-  rendering('oda_field', 'nVfuSmsj9cULg3712chv', res, 0);
+  rendering('oda_field', '織田フィールド', 'nVfuSmsj9cULg3712chv', res, 0);
 });
 
 router.get('/todoroki', function(req, res, next) {
-  rendering('todoroki', '67c7uxgRWDkxr1S4gPaR', res, 0);
+  rendering('todoroki', '等々力陸上競技場', '67c7uxgRWDkxr1S4gPaR', res, 0);
 });
 
 router.get('/yumenoshima', function(req, res, next) {
-  rendering('yumenoshima', 'VFurPbbeejEbtu1JNTzF', res, 0)
+  rendering('yumenoshima', '夢の島陸上競技場', 'VFurPbbeejEbtu1JNTzF', res, 0)
 });
 
 router.get('/toukyou_taiikukan', function(req, res, next) {
-  rendering('toukyou_taiikukan', 'Xd7KDIkcO0Rsroho5tBq', res, 0)
+  rendering('toukyou_taiikukan', '東京体育館', 'Xd7KDIkcO0Rsroho5tBq', res, 0)
 });
 
 router.get('/komazawa_olympic', function(req, res, next) {
-  rendering('komazawa_olympic', 'WrrQXe67xvIkGfMtJ51E', res, 0)
+  rendering('komazawa_olympic', '駒沢オリンピック公園陸上競技場', 'WrrQXe67xvIkGfMtJ51E', res, 0)
 });
 
 router.get('/edogawaku', function(req, res, next) {
-  rendering('edogawaku', 'pViuyE86zXO1q2mMSMtI', res, 0)
+  rendering('edogawaku', '江戸川区陸上競技場', 'pViuyE86zXO1q2mMSMtI', res, 0)
 });
 
 router.get('/ooihuto', function(req, res, next) {
-  rendering('ooihuto', 'TF8vwhZ8d1pB3fOtLMRt', res, 0)
+  rendering('ooihuto', '大井ふ頭中央海浜公園スポーツの森陸上競技場', 'TF8vwhZ8d1pB3fOtLMRt', res, 0)
 });
 
 router.get('/nissan', function(req, res, next) {
-  rendering('nissan', 'hcRDuE9rpZfwNlpuIcJ0', res, 0)
+  rendering('nissan', '日産スタジアム', 'hcRDuE9rpZfwNlpuIcJ0', res, 0)
 });
 
-function rendering(page_name, stadiumId, res, index) {
+function rendering(page_name, stadiumName, stadiumId, res, index) {
   res.render(page_name, {
+    title: 'いつラン - ' + stadiumName + 'を個人利用する人のための利用時間確認ページ',
+    description: stadiumName + '等の陸上競技場を個人利用したい際に、このサイトにて開放日・利用可能時間が確認できます。',
     stadium_id: stadiumId,
     week_index: index,
   })
