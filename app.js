420bb3609933feec957f7c61fbbbd6583f8e7d5b
subaru44k <odsubaru@gmail.com>
2018-07-01 12:51:22 +0900

    Tentative commit to add pace list

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var manageRouter = require('./routes/manage');
+var paseRouter = require('./routes/pace');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/manage', manageRouter);
+app.use('/pace', paseRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
