68fdb969587c5adb49e9f627063aa65db08cbd70
subaru44k <odsubaru@gmail.com>
2018-07-02 23:28:04 +0900

    Modify title

--- routes/pace.js
+++ routes/pace.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/marathon', function(req, res, next) {
   res.render('pacelist', {
-    title: 'いつラン - マラソンのペース表。5kmごとのラップ表記。',
+    title: 'いつラン - マラソンのペース表。5kmごとのラップタイム表記。',
     description: 'マラソンの5kmごとのラップタイムがひと目で分かります。サブスリー、サブフォー、サブファイブ、2時間の世界記録まで。スマートフォン対応。'
   })
 });
